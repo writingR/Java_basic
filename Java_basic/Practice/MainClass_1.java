e4aa893bf7c85b9ba860f7f7df4dd702bc98d57d
writingr <wonjobono0625@gmail.com>
2019-08-22 23:05:25 +0900

    Hello World.

---
+++ Java_basic/Practice/MainClass_1.java
@@ -0,0 +1,21 @@
+package java_basic;
+
+/*
+*    ****** 중요 *******
+*    Hello World. 출력하려면 ? 
+*    
+*    - java 개발을 위해선 JDK가 필요하다. 프로그램만을 사용한다면 JRE만 설치하면 된다.
+*  	 - 개발을 위해 JDK를 설치했다면, 환경변수 설정을 해준다.
+*    - 그리고 통합 개발 환경을 제공하는 이클립스를 설치하여 개발 환경을 구축해 준다. 
+*    - 클래스를 만들고 출력준비를 마친다!!!
+*/
+
+public class MainClass_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World.");
+		
+	}
+	
+}
